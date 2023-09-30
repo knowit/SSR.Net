f6cfabf67e878cdb0dd7a91322c1f735abf8490e
Øivind Hagen <oivind.hagen@knowit.no>
2023-09-30 21:06:42 +0200

    Finish readme, clean up code.

--- src/SSR.Net.DotNet6/Controllers/HomeController.cs
+++ src/SSR.Net.DotNet6/Controllers/HomeController.cs
@@ -2,18 +2,15 @@
 using Newtonsoft.Json;
 using SSR.Net.Services;
 
-namespace SSR.Net.DotNet6.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace SSR.Net.DotNet6.Controllers {
+    public class HomeController : Controller {
         private readonly React17Renderer _react17Renderer;
         private readonly React18Renderer _react18Renderer;
         private readonly Vue3Renderer _vue3Renderer;
 
         public HomeController(React17Renderer react17Renderer,
                               React18Renderer react18Renderer,
-                              Vue3Renderer vue3Renderer)
-        {
+                              Vue3Renderer vue3Renderer) {
             _react17Renderer = react17Renderer;
             _react18Renderer = react18Renderer;
             _vue3Renderer = vue3Renderer;
@@ -21,33 +18,30 @@ namespace SSR.Net.DotNet6.Controllers
 
         public ActionResult Index() => View();
 
-        public ActionResult React17() =>
-            View(_react17Renderer.RenderComponent(
-            "Components.FrontPage",
-            JsonConvert.SerializeObject(
-                new
-                {
+        public ActionResult React17() {
+            var propsJson = JsonConvert.SerializeObject(
+                new {
                     header = "React 17 with SSR",
                     links = new[]{
-                    new {
-                        text = "Google.com",
-                        href ="https://www.google.com"
-                    },
-                    new {
-                        text = "Hacker news",
-                        href = "https://news.ycombinator.org"
+                        new {
+                            text = "Google.com",
+                            href ="https://www.google.com"
+                        },
+                        new {
+                            text = "Hacker news",
+                            href = "https://news.ycombinator.org"
+                        }
                     }
-                }
-                })));
+                });
+            var renderedComponent = _react17Renderer.RenderComponent("Components.FrontPage", propsJson);
+            return base.View(renderedComponent);
+        }
 
-        public ActionResult React18() =>
-            View(_react18Renderer.RenderComponent(
-                "Components.FrontPage",
-                JsonConvert.SerializeObject(
-                    new
-                    {
-                        header = "React 18 with SSR",
-                        links = new[]{
+        public ActionResult React18() {
+            var propsJson = JsonConvert.SerializeObject(
+                new {
+                    header = "React 18 with SSR",
+                    links = new[]{
                         new {
                             text = "Google.com",
                             href = "https://www.google.com"
@@ -57,16 +51,18 @@ namespace SSR.Net.DotNet6.Controllers
                             href = "https://news.ycombinator.org"
                         }
                     }
-                    })));
+                });
+            var renderedComponent = _react18Renderer.RenderComponent("Components.FrontPage", propsJson);
+            return View(renderedComponent);
+        }
 
-        public ActionResult Vue3() =>
-            View(_vue3Renderer.RenderComponent(
-                "Components.Example",
-                JsonConvert.SerializeObject(
-                    new
-                    {
-                        title = "Vue 3 with SSR"
-                    }
-                )));
+        public ActionResult Vue3() {
+            var propsJson = JsonConvert.SerializeObject(
+                new {
+                    title = "Vue 3 with SSR"
+                });
+            var renderedComponent = _vue3Renderer.RenderComponent("Components.Example", propsJson);
+            return View(renderedComponent);
+        }
     }
 }
